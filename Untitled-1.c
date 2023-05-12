3b618f5aa5841a6f0958f72de6668596a47af9b6
florian-lemesle <florian.lemesle@laplateforme.io>
2023-05-12 09:01:57 +0200

    enter exit

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    printf("Hello, World!\n");
+    printf("Press any key to exit...");
+    getchar(); // waits for user to press any key
     return 0;
 }
