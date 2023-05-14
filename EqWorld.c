8ccb299bd7a7f013cb69ac83ee591ded74607b4e
florian-lemesle <florian.lemesle@laplateforme.io>
2023-05-14 19:55:01 +0200

    14/05

--- EqWorld.c
+++ EqWorld.c
@@ -1,30 +1,44 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void compare(int x, int y)
 {
-  if (x < y) {
-    printf("%i is less than %i\n",x,y);
+  if (x < y)
+  {
+    printf("%i is less than %i\n", x, y);
   }
-  else if (x > y) {
-    printf("%i is more than %i\n",x,y);
+  else if (x > y)
+  {
+    printf("%i is more than %i\n", x, y);
   }
-  else {
-    printf("%i is equal to %i\n",x,y);
+  else
+  {
+    printf("%i is equal to %i\n", x, y);
   }
 }
 
-int main(void)
+int main()
 {
   int num1, num2;
-  
-  printf("Enter the first number: ");
-  scanf("%d", &num1);
-  
-  printf("Enter the second number: ");
-  scanf("%d", &num2);
 
-  compare(num1, num2);
+  char c;
 
-  return 0;
+  while (c != 'q')
+  {
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    compare(num1, num2);
+
+    printf("Press q to quit or press enter to start again!\n");
+
+    scanf("%d", &c);
+
+  }
+
+  
 }
