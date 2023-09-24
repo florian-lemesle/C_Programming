8dfcbb2583e1349cc0e3255012526381294ecc40
florian-lemesle <florian.lemesle@laplateforme.io>
2023-09-24 11:09:16 +0200

    age

--- ocr.c
+++ ocr.c
@@ -3,9 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-  int nombreDeVies = 5, niveau = 1;
-  
-  printf("Vous avez %d vies et vous etes au niveau %d\n", nombreDeVies, niveau);
+  int age = 0;
+
+  printf("Quel age avez-vous?");
+  scanf("%d", &age);
+  printf("Vous avez %d ans", age);
   
   return 0;
 }
