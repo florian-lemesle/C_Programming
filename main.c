dd4f77409f99af3577640f23271da9680c593adf
florian-lemesle <florian.lemesle@laplateforme.io>
2023-10-05 16:54:50 +0200

    td

--- main.c
+++ main.c
@@ -3,9 +3,20 @@
 
 int main() {
 
-    int a;
-    a = 55;
+    int a, i, n;
+    
+    printf("Veuillez saisir un nombre\n");
+    scanf("%d", &n);
 
-    printf("a = %x", a);
+    for (a=1, i=0; i<n ; i++)
+    {
+        a = a*(i+1);
+
+    }
+
+     printf("la factorielle est %d\n", a);
+    
+    return 0;
+
 
 }
